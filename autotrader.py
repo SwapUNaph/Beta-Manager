2fdfd9937bbe9e481c41c125aa58e1f7ba4e316c
Swapneel Naphade <naphadeswapneeel@gmail.com>
2019-03-11 14:49:16 -0400

    Webapp started

--- autotrader.py
+++ autotrader.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
-import quandl
-quandl.ApiConfig.api_key = 'yDsZNbJArbvQby72sxM9'
+from datetime import datetime
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
 
-portfolio_df = pd.read_excel('portfolio.xlsx').set_index('stock')
+
 start_date = '2014-01-01'
 end_date = '2019-01-01'
 
@@ -14,7 +15,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_portfolio():
-   return render_template('index.html', table = portfolio_df.to_html())
+   return render_template('index.html')
+
+
+@app.route('/result', methods = ['POST','GET'])
+def result():
+    if request.method == 'POST':
+        print(request.form)
+        stocks = request.form['stocks'].split()
+        start_date = datetime.strptime(request.form['start_date'], '%Y-%m-%d')
+        window = int(request.form['window'])
+        print(stocks)
+        return render_template('results.html', stocks=stocks, start_date=start_date, window=window)
 
 if __name__ == '__main__':
    app.run(debug=True)
